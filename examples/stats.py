6bc5a74966ec8e4333144e686c3abf66a3df5727
Dice <diceroll123@gmail.com>
2020-08-02 12:40:28 -0400

    Fix broken example

--- examples/stats.py
+++ examples/stats.py
@@ -7,7 +7,7 @@ async def main():
     dti_client = Client()
     sorted_species = sorted(await dti_client.all_species(), key=lambda s: s.name)
     for species in sorted_species:
-        print(f"{species.name} can have this many colors: {len(species.colors())}")
+        print(f"{species.name} can have this many colors: {len(await species.colors())}")
 
 
 asyncio.run(main())
