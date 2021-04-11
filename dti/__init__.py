5518c6e709f05634ecdbb996afb23cb614369ae5
Steve C <diceroll123@gmail.com>
2021-04-11 19:35:22 -0400

    Update copyright

--- dti/__init__.py
+++ dti/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "dti"
 __author__ = "diceroll123"
 __license__ = "MIT"
-__copyright__ = "Copyright 2020 diceroll123"
+__copyright__ = "Copyright 2020-present diceroll123"
 __version__ = "0.0.1a"
 
 from .client import Client
