60cde9da42f2a8fd7ccd2a24d02f73770bc4302c
Steve C <diceroll123@gmail.com>
2022-02-27 16:14:41 -0500

    Add typing stub

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
     description="A Python wrapper for the Dress To Impress API",
     python_requires=">=3.7",
     packages=["dti", "dti.types"],
+    include_package_data=True,
     install_requires=requirements,
     extras_require=extras_require,
 )
