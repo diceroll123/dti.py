b679b396b465b98fee8124cd679062f964544dc6
Steve C <diceroll123@gmail.com>
2026-01-03 15:38:17 -0500

    Add new outfit render url

--- dti/utils.py
+++ dti/utils.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
-from urllib.parse import quote, urljoin, urlparse
+from typing import Any
+from urllib.parse import quote, urlencode, urljoin, urlparse
 
-from .enums import LayerImageSize
+from .enums import LayerImageSize, PetPose
 
 __all__: tuple[str, ...] = (
     "build_layers_url",
+    "outfit_image_url",
     "url_sanitizer",
 )
 
@@ -29,6 +31,59 @@ def build_layers_url(
     joined = ",".join(quote(layer) for layer in layers)
 
     return f"https://impress-2020.openneo.net/api/outfitImage?size={size_str}&layerUrls={joined}"
+
+
+def outfit_image_url(
+    *,
+    species: int | str,
+    color: int | str,
+    pose: PetPose,
+    style: int | None = None,
+    name: str | None = None,
+    item_ids: list[int] | None = None,
+) -> str:
+    """
+    Convenience method to get the image url for a DTI outfit.
+
+    The size of the image will be 600x600, not configurable.
+
+    Parameters
+    ----------
+    species: :class:`int` | :class:`str`
+        The species ID of the pet.
+    color: :class:`int` | :class:`str`
+        The color ID of the pet.
+    pose: :class:`PetPose`
+        The desired pet pose for the render.
+    style: :class:`int` | None
+        The alt style ID of the pet.
+    name: :class:`str` | None
+        The name of the pet.
+    item_ids: :class:`list`[ :class:`int`] | None
+        A list of item ids to add to the pet.
+
+    Returns
+    -------
+    :class:`str`
+        The image url for the pet appearance.
+    """
+    parts: dict[str, Any] = {
+        "species": species,
+        "color": color,
+        "pose": pose.name,
+    }
+    if name:
+        # TODO: is this needed?
+        parts["name"] = name
+
+    if style:
+        parts["style"] = style
+
+    item_ids = item_ids or []
+    if item_ids:
+        parts["objects[]"] = [str(item_id) for item_id in item_ids]
+
+    return f"https://impress.openneo.net/outfits/new.png?{urlencode(parts, doseq=True)}"
 
 
 def url_sanitizer(url: str, /) -> str:
---
+++ tests/test_utils.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from dti.enums import PetPose
+from dti.utils import outfit_image_url
+
+
+def test_outfit_image_url() -> None:
+    url = outfit_image_url(
+        species=1,
+        color=8,
+        pose=PetPose.HAPPY_FEM,
+        style=90030,
+        item_ids=[74967, 37002, 71526],
+    )
+    assert (
+        url
+        == "https://impress.openneo.net/outfits/new.png?"
+        "species=1&color=8&pose=HAPPY_FEM&style=90030&"
+        "objects%5B%5D=74967&objects%5B%5D=37002&objects%5B%5D=71526"
+    )
